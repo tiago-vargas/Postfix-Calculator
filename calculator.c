f7d4e48a5e50dccc78347185950d303dd9829f1d
Tiago Vargas <tiago.ce10@outlook.com>
2022-09-09 00:11:45 -0300

    temp file; delete it later

--- calculator.c
+++ calculator.c
@@ -1,1 +1,25 @@
+#include <stdlib.h>
+#include "../FloatStack/float_stack.c"
 
+float evaluate(char *input)
+{
+	FloatStack stack;
+	char *element;
+
+	int index = 0;
+	element = get_next_element(input, index);
+
+	if (is_operator(element))
+	{
+	}
+	else if (is_number(element))
+	{
+		float number = atof(element);
+		push_float(number, &stack);
+	}
+	else
+	{
+		fprintf(stderr, "invalid syntax: %s", element);
+	}
+
+}
--- test_calculator.c
+++ test_calculator.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "calculator.c"
 
-void run_tests()
-{}
+bool test_single_operation()
+{
+	char *input = "2 5 +";
+
+	float result = evaluate(input);
+
+	return result == 7.0f;
+}
+
+void run_test(bool (*testing_function)(), char *message)
+{
+	if (testing_function())
+		printf("\x1b[32mPASSED\x1b[0m: %s\n", message);
+	else
+		printf("\x1b[31mFAILED\x1b[0m: %s\n", message);
+}
+
+void run_all_tests()
+{
+	printf("-- TEST RESULTS --\n");
+
+	run_test(test_single_operation, "`2 5 +` should return 7");
+}
 
 void main()
 {
-	run_tests();
+	run_all_tests();
 }
