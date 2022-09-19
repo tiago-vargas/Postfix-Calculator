36d50b4017f76f66897146baec1494096b722481
Tiago Vargas <tiago.ce10@outlook.com>
2022-09-19 18:13:44 -0300

    feat: evaluate single addition

--- calculator.c
+++ calculator.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include "../../FloatStack/float_stack.c"
 
+float evaluate(char input[])
+{
+	float operand_1, operand_2;
+
+	char *tokens[200];
+	tokens[0] = strtok(input, " ");
+	tokens[1] = strtok(NULL, " ");
+	tokens[2] = strtok(NULL, " ");
+
+	operand_1 = atof(tokens[0]);
+	operand_2 = atof(tokens[1]);
+
+	return operand_1 + operand_2;
+}
