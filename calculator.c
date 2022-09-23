160024c97cd3bda5fee6c9ea1f2832bfb52767c2
Tiago Vargas <tiago.ce10@outlook.com>
2022-09-23 00:52:03 -0300

    refact: use tokenize function

--- calculator.c
+++ calculator.c
@@ -2,14 +2,20 @@
 #include <string.h>
 #include "../../FloatStack/float_stack.c"
 
+void tokenize(char string[], char *tokens[])
+{
+	tokens[0] = strtok(string, " ");
+
+	for (int i = 1; i < 200; i++)
+		tokens[i] = strtok(NULL, " ");
+}
+
 float evaluate(char input[])
 {
 	float operand_1, operand_2;
 
 	char *tokens[200];
-	tokens[0] = strtok(input, " ");
-	tokens[1] = strtok(NULL, " ");
-	tokens[2] = strtok(NULL, " ");
+	tokenize(input, tokens);
 
 	operand_1 = atof(tokens[0]);
 	operand_2 = atof(tokens[1]);
