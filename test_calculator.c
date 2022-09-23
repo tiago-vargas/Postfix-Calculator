b416721a26b1515afcb20b5e966d649146623489
Tiago Vargas <tiago.ce10@outlook.com>
2022-09-23 00:37:08 -0300

    feat: support with floating point numbers

--- test_calculator.c
+++ test_calculator.c
@@ -44,6 +44,27 @@ bool test_single_integer_division(out float *actual_result)
 	return result == 5;
 }
 
+bool test_float_operands(out float *actual_result)
+{
+	char input[] = "5.6 1.9 +";
+
+	float result = evaluate(input);
+
+	*actual_result = result;
+	return result == 7.5f;
+}
+
+
+bool test_float_division_result(out float *actual_result)
+{
+	char input[] = "5 2 /";
+
+	float result = evaluate(input);
+
+	*actual_result = result;
+	return result == 2.5f;
+}
+
 void run_test(bool (*testing_function)(), char *message)
 {
 	float actual_result;
@@ -62,6 +83,9 @@ void run_all_tests()
 	run_test(test_single_subtraction, "`2 5 -` should evaluate to -3");
 	run_test(test_single_multiplication, "`2 5 *` should evaluate to 10");
 	run_test(test_single_integer_division, "`10 2 /` should evaluate to 5");
+
+	run_test(test_float_operands, "`5.6 1.9 +` should evaluate to 7.5f");
+	run_test(test_float_division_result, "`5 2 /` should evaluate to 2.5f");
 }
 
 void main()
